8ea74dec998039b202334abfcae6475a97e6428d
tinysheep007 <pzxiaomie@gmail.com>
2021-08-05 16:17:39 -0400

    add name in each color

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 import './ColorBox.css';
 class ColorBox extends Component {
     render() {
+        const {name, background} = this.props;
         return (
-            <div className="ColorBox"  style={{background: this.props.background}}>
-                <span>{this.props.name}</span>
+            <div className="ColorBox"  style={{background}}>
+                <div className="copy-container">
+                    <div className="box-content"> 
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <div className="see-more">More</div>
             </div>
 
 
