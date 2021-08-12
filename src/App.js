fc3267b4f3c02c94d7cdc951fb8f514683ae9160
tinysheep007 <pzxiaomie@gmail.com>
2021-08-12 17:01:57 -0400

    able to generate colors of 10 levels

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import seedColors from './seedColors';
 import Palette from './Palette';
-
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div className="App">
         <Palette {...seedColors[1]}/>
