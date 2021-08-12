1e69c775c70a868093e4bae28f2677c47cc48ea0
tinysheep007 <pzxiaomie@gmail.com>
2021-08-12 17:11:31 -0400

    able to gen different shades of color

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,45 +1,49 @@
-import chroma from 'chroma-js';
+import chroma from "chroma-js";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-let levels = [50,100,200,300,400,500,600,700,800,900];
-
-function generatePalette(starter){
-    let newPalette = {
-        paletteName : starter.paletteName,
-        id : starter.id,
-        emoji : starter.emoji,
-        colors:{}
+function generatePalette(starterPalette) {
+  let newPalette = {
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
+    colors: {}
+  };
+  for (let level of levels) {
+    newPalette.colors[level] = [];
+  }
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i])
+          .css()
+          .replace("rgba", "rgba")
+          .replace(")", ",1.0)")
+      });
     }
-    for(let level of levels){
-        newPalette.colors[level]=[];
-    }
-    for(let color of starter.colors){
-        let scale=getScale(color.color, 10).reverse();
-        for(let i in scale){
-            newPalette.colors[levels[i]].push({
-                name: `${color.name} ${levels[i]}`,
-                id: color.name.toLowerCase().replace(/  /g, "-"),
-                hex:scale[i],
-                rgb: chroma(scale[i]).css(),
-                rgba: chroma(scale[i]).css().replace("rgba","rgba").replace(")",",1.0)")
-            });
-            
-        }
-    }
-    return newPalette;
-
+  }
+  return newPalette;
+}
+function getRange(hexColor) {
+  const end = "#fff";
+  return [
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
+    hexColor,
+    end
+  ];
 }
 
-function getRange(hexColor){
-    const end="#fff";
-    return [
-        chroma(hexColor).darken(1.4).hex(),
-        hexColor,
-        end
-    ];
+function getScale(hexColor, numberOfColors) {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
 }
 
-function getScale (hexColor, numOfColor){
-    return chroma.scale(getRange(hexColor)).mode("lab").colors(numOfColor);
-}
-
-export { generatePalette }
+export { generatePalette };
