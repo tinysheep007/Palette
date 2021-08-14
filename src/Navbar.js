3e1d9b3f40f664037c71072fe91ad799375fb4bf
tinysheep007 <pzxiaomie@gmail.com>
2021-08-14 09:48:11 -0400

    added select bars on top

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Navbar.css'
+import './Navbar.css';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
 class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state={ format: "hex"};
+        this.handleChange=this.handleChange.bind(this);
+
+    }
+    handleChange(e){
+        this.setState({ format: e.target.value});
+        this.props.handleChange(e.target.value);
+    }
     render(){
-        const {level, changeLevel} = this.props;
+        const {level, changeLevel, handleChange} = this.props;
+        const { format } = this.state;
         return (
             <div className="Navbar">
                 <div className="logo">
@@ -22,7 +37,14 @@ class Navbar extends Component {
                         />
                     </div>
                 </div>
-                
+                <div className="selector">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">hex (#fffff)</MenuItem>
+                        <MenuItem value="rgb">rgb (255,255,255)</MenuItem>
+                        <MenuItem value="rgba">rgba (255,255,255,1.0)</MenuItem>
+                    </Select>
+
+                </div>
             </div>
 
         )
