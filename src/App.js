f12a7f66f22d29fe8d71e9df29dbbdbb1a774456
tinysheep007 <pzxiaomie@gmail.com>
2021-08-14 10:47:22 -0400

    added basic router that doesnt work

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 import seedColors from './seedColors';
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     
     return (
       <div className="App">
+        <Switch>
+          <Route exact path="/" render={()=> <h1>main page</h1>} />
+          <Route exact path="/palette/:id" render={()=> <h1>palatte</h1>}/>
+        </Switch>
         <Palette palette={generatePalette(seedColors[4])}/>
 
       </div>
