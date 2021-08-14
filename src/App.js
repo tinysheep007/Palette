5d47083c5a70a6e84aca7d38f7c7b9e624dd8696
tinysheep007 <pzxiaomie@gmail.com>
2021-08-14 10:57:44 -0400

    added route where u need /palette/:id

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ import { generatePalette } from './colorHelpers';
 import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id===id;
+    });
+  }
+
   render() {
     
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" render={()=> <h1>main page</h1>} />
-          <Route exact path="/palette/:id" render={()=> <h1>palatte</h1>}/>
+          <Route exact path="/palette/:id" render={(routeProps)=> (
+            <Palette 
+              palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+            />
+          )}/>
         </Switch>
-        <Palette palette={generatePalette(seedColors[4])}/>
+        {/* <Palette palette={generatePalette(seedColors[4])}/> */}
 
       </div>
     );
