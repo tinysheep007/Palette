270b4ddb17a382a1962b3147b28ea3da41640a95
tinysheep007 <pzxiaomie@gmail.com>
2021-08-17 19:48:51 -0400

    add delete button on new palette

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles= {
     root:{
@@ -10,12 +11,38 @@ const styles= {
         cursor: "pointer",
         position: "relative",
         marginBottom: "-4px",
+        "&:hover svg":{
+            color:"black",
+            transform:"scale(1.5)"
+        }
+    },
+    boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "15px",
+        display:"flex",
+        justifyContent:"space-between"
+    },
+    deleteIcon:{
+        color:"white",
+        transition:"all 0.3s ease-in-out"
     }
 }
 function DraggableColorBox(props){
+    const { classes } =  props;
     return (
-        <div className={props.classes.root} style={{backgroundColor : props.color}}>
-            {props.name}
+        <div className={classes.root} style={{backgroundColor : props.color}}>
+            <div className={classes.boxContent}>
+                <span>{props.name}</span>
+                <DeleteIcon className={classes.deleteIcon}/>
+            </div>
+            
         </div>
     )
 }
